dcc454d5deb1048465940e9a761f94544c2af2a1
Pavlo Babenko <pavlo.babenko@gmail.com>
2015-07-28 14:16:33 +0300

    add: blueprints

--- index.js
+++ index.js
@@ -2,5 +2,19 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-f7'
+  name: 'ember-cli-f7',
+
+  included: function(app) {
+    this._super.included(app);
+
+    var bowerDirectory = app.bowerDirectory;
+    app.import(bowerDirectory + '/framework7/dist/css/framework7.ios.css');
+    app.import(bowerDirectory + '/framework7/dist/css/framework7.ios.colors.css');
+    app.import(bowerDirectory + '/framework7/dist/css/framework7.material.css');
+    app.import(bowerDirectory + '/framework7/dist/css/framework7.material.colors.css');
+    app.import(bowerDirectory + '/framework7/dist/js/framework7.js');
+    app.import(bowerDirectory + '/framework7/dist/js/framework7.js.map', {
+      destDir: 'assets'
+    });
+  }
 };
